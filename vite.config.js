edae8955b74095900ca81284fa9b1cd25a0652c6
sosarodrigox <sosarodrigox@gmail.com>
2023-08-27 20:02:33 -0300

    solucionado env local

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Define las variables de entorno para Vite
-const viteEnv = {
-  VITE_REACT_APP_BACKEND_URL: process.env.VITE_REACT_APP_BACKEND_URL || 'http://localhost:8000',
-};
-
 export default defineConfig({
   plugins: [react()],
-  define: viteEnv,
 });
