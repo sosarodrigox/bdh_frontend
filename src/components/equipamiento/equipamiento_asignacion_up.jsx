e8a3bd7baebb14d5deeb21db0d2c858ef84f51c1
sosarodrigox <sosarodrigox@gmail.com>
2023-09-14 20:14:27 -0300

    corregido asignacion equipamiento

--- src/components/equipamiento/equipamiento_asignacion_up.jsx
+++ src/components/equipamiento/equipamiento_asignacion_up.jsx
@@ -1,178 +1,108 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-// import EmprendimientoIndividual from "./emprendimiento_individual_up";
-// import GrupoAsociativo from "./grupo_asociativo_up";
-// import Cooperativa from "./cooperativa_up";
-// import Formulario_UP from "./formulario_up";
 
 export default function EquipamientoAsignacionUP() {
-    const [persona, setPersona] = useState({});
-    const [tipoUnidadProductiva, setTipoUnidadProductiva] = useState("");
-    const [nombreGrupo, setNombreGrupo] = useState("");
-    const [nombreCooperativa, setNombreCooperativa] = useState("");
-
-
-    const [unidadProductiva, setUnidadProductiva] = useState({
-        persona_id: 0,
-        denominacion_up: "",
-        tipo_up: "",
-        antiguedad_emprendimiento_meses: 0,
-        antiguedad_emprendimiento_anios: 0,
-        emprendimiento_formalizado: false,
-        emprendimiento_activo: true,
-        comercializacion_descripcion: "",
-        servicios_productos: "",
-        cantidad_integrantes: 0,
-    });
+    const [equipamientos, setEquipamientos] = useState([]);
+    const [equipamientoSeleccionado, setEquipamientoSeleccionado] = useState({});
+    const [up, setUP] = useState({});
 
     const params = useParams();
     const navegar = useNavigate();
 
     useEffect(() => {
-        getPersona(params.id);
-    }, [params.id]);
+        getUP(params.id);
+        getEquipamientos();
+    }, []);
 
-    const getPersona = async (id) => {
+    const getUP = async (id) => {
         try {
-            let resultado = await axios.get(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/personas/${id}`);
-            setPersona(resultado.data);
+            let resultado = await axios.get(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/up/${id}`);
+            setUP(resultado.data);
         } catch (error) {
             console.log(error);
-            setPersona({});
+            setUP({});
         }
     };
 
-    const handleChange = (event) => {
-        setTipoUnidadProductiva(event.target.value);
+    const getEquipamientos = async () => {
+        try {
+            let resultado = await axios.get(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/equipamiento/`);
+            setEquipamientos(resultado.data);
+        } catch (error) {
+            setEquipamientos([]);
+            console.error(error);
+            alert(error.response.data.detail);
+        }
     };
 
-    const grabarCambios = async () => {
-        if (!tipoUnidadProductiva) {
-            alert("Debe seleccionar un tipo de unidad productiva");
-            return;
-        }
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        const equipamientoSeleccionadoId = parseInt(e.target.value);
 
-        if (persona.rol !== "no asignado") {
-            alert("La persona ya tiene asignada una unidad productiva");
-            return;
+        if (equipamientoSeleccionadoId === -1) {
+            console.log('equipamientoSeleccionadoId === -1');
+            setEquipamientoSeleccionado({});
         } else {
+            console.log('equipamientoSeleccionadoId !== -1');
+            console.log(equipamientoSeleccionadoId);
+            const equipamientoSeleccionado = equipamientos.find((eq) => eq.id === equipamientoSeleccionadoId);
+            setEquipamientoSeleccionado(equipamientoSeleccionado);
+        }
+    };
 
-            try {
-                if (tipoUnidadProductiva === "EMPRENDIMIENTO INDIVIDUAL") {
-                    await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/emprendedores`, {
-                        persona_id: persona.id,
-                    });
-
-                    unidadProductiva.denominacion_up = `UP_${persona.apellido}_${persona.cuil}`;
-                    unidadProductiva.persona_id = persona.id;
-                    unidadProductiva.tipo_up = 'emprendedor';
-                    await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/up`, unidadProductiva);
-
-                    persona.rol = "emprendedor"
-                    let resultado = await axios.put(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/personas/${persona.id}`, persona);
-                    console.log(resultado);
+    const asignarEquipamiento = async () => {
+        console.log("asignarEquipamiento");
+        try {
+            console.log("try");
+            if (!Object.keys(equipamientoSeleccionado).length) {
+                console.log("if");
+                alert("Debe seleccionar el equipamiento");
+            } else {
+                console.log("else");
+                let nuevoEquipamiento = {
+                    "id_equipamiento": equipamientoSeleccionado.id,
+                    "id_up": up.id,
+                    "cantidad": 1.0,
+                    "valor_total": 10.00
                 }
-                // else {
-                //     // // Realiza la solicitud para crear o actualizar la unidad productiva
-                //     // await axios.post("http://localhost:8000/up", unidadProductiva);
-                // }
+                console.log(nuevoEquipamiento);
+                await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/asignaciones`, nuevoEquipamiento);
 
-                if (tipoUnidadProductiva === "GRUPO ASOCIATIVO") {
-                    await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/grupos`, {
-                        representante_grupo_id: persona.id,
-                        nombre_grupo: nombreGrupo,
-                    });
-
-                    unidadProductiva.denominacion_up = `UP_${nombreGrupo}_${persona.cuil}`;
-                    unidadProductiva.persona_id = persona.id;
-                    unidadProductiva.tipo_up = 'grupo';
-                    await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/up`, unidadProductiva);
-
-                    persona.rol = "representante"
-                    let resultado = await axios.put(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/personas/${persona.id}`,
-                        persona);
-                    console.log(resultado);
-                }
-
-                if (tipoUnidadProductiva === "COOPERATIVA") {
-                    await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/cooperativas`, {
-                        presidente_id: persona.id,
-                        nombre_cooperativa: nombreCooperativa,
-                    });
-
-                    unidadProductiva.denominacion_up = `UP_${nombreCooperativa}_${persona.cuil}`;
-                    unidadProductiva.persona_id = persona.id;
-                    unidadProductiva.tipo_up = 'cooperativa';
-                    await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/up`, unidadProductiva);
-
-                    persona.rol = "presidente"
-                    let resultado = await axios.put(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/personas/${persona.id}`,
-                        persona);
-                    console.log(resultado);
-                }
-                //TODO: Corregir error al asignar una unidad productiva a una persona no aparece el nombre de la unidad productiva en el alert si no "undefined"
-                alert(`La Persona: ${persona.apellido}, ${persona.nombre} a sido asignada exitosamente a la Unidad Productiva tipo: ${persona.rol} con el nombre de UP: ${unidadProductiva.denominacion_up}.`);
+                alert(`La Unidad Productiva ${up.denominacion_up}, a sido equipada con: ${equipamientoSeleccionado.descripcion_principal}`);
                 navegar(-1);
-            } catch (error) {
-                console(error.response.data.detail);
             }
+        } catch (error) {
+            console.log("catch");
+            alert(error.response.data.detail);
         }
     };
 
     return (
         <div className="text-start col-6 offset-3 border p-3">
-            <h1 className="mt-3 text-center">Asignar Equipamiento:</h1>
+            <h1 className="mt-3 text-center">Asignar Equipamiento a UP:</h1>
             <h3 className="mt-3 text-center">
-                {persona.apellido}, {persona.nombre} - {persona.cuil}
+                {up.tipo_up} - {up.denominacion_up}
             </h3>
-            <div className="mb-3 col-2-center">
-                <select
-                    className="form-control"
-                    id="edTipoUP"
-                    name="edTipoUP"
-                    value={tipoUnidadProductiva}
+            <div className="mb-3">
+                <label className="form-label">Listado de Equipamiento:</label>
+                <select className="form-control"
+                    id="listadoEquipamiento"
+                    name="listadoEquipamiento"
+                    value={equipamientoSeleccionado.id || -1}
                     onChange={handleChange}
                 >
-                    <option value="">Seleccione un tipo de unidad productiva</option>
-                    <option value="EMPRENDIMIENTO INDIVIDUAL">EMPRENDIMIENTO INDIVIDUAL</option>
-                    <option value="GRUPO ASOCIATIVO">GRUPO ASOCIATIVO</option>
-                    <option value="COOPERATIVA">COOPERATIVA</option>
+                    <option value="-1">Seleccione equipamiento</option>
+                    {equipamientos.map((equipamiento) => (
+                        <option key={equipamiento.id} value={equipamiento.id}>
+                            {equipamiento.descripcion_principal}
+                        </option>
+                    ))}
                 </select>
             </div>
 
-            {tipoUnidadProductiva === "EMPRENDIMIENTO INDIVIDUAL" && (
-                <>
-                    <EmprendimientoIndividual persona={persona} />
-                    <Formulario_UP
-                        unidadProductiva={unidadProductiva}
-                        setUnidadProductiva={setUnidadProductiva}
-                    />
-                </>
-            )}
-
-            {tipoUnidadProductiva === "GRUPO ASOCIATIVO" && (
-                <GrupoAsociativo
-                    persona={persona}
-                    unidadProductiva={unidadProductiva}
-                    setUnidadProductiva={setUnidadProductiva}
-                    nombreGrupo={nombreGrupo}
-                    setNombreGrupo={setNombreGrupo}
-                />
-
-            )}
-
-            {tipoUnidadProductiva === "COOPERATIVA" && (<Cooperativa
-                persona={persona}
-                unidadProductiva={unidadProductiva}
-                setUnidadProductiva={setUnidadProductiva}
-                nombreCooperativa={nombreCooperativa}
-                setNombreCooperativa={setNombreCooperativa}
-            />
-            )}
-
             <div className="mb-3 text-end">
-                <button className="btn btn-primary me-1" onClick={grabarCambios}>
+                <button className="btn btn-primary me-1" onClick={asignarEquipamiento}>
                     Aceptar
                 </button>
                 <button className="btn btn-secondary ms-1" onClick={() => navegar(-1)}>
